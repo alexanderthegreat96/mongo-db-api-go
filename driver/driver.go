8035cde1ba966cbf3139521480c0c56255d15bbc
Alexandru <alexandrulupaescu@gmail.com>
2024-04-21 19:27:40 +0300

    continued improving the driver.go

--- driver/driver.go
+++ driver/driver.go
@@ -2,20 +2,23 @@ package driver
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"strconv"
 	"time"
 
 	"github.com/alexanderthegreat96/mongo-db-api-go/helpers"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoHandler struct {
 	debug         bool
-	limit         int64
-	perPage       int64
-	page          int64
-	sort          []string
+	limit         int
+	perPage       int
+	page          int
+	sort          []primitive.E
 	query         map[string]interface{}
 	host          string
 	port          string
@@ -30,6 +33,37 @@ type MongoHandler struct {
 	collection    *mongo.Collection
 }
 
+// used for results mapping
+type MongoResultPagination struct {
+	TotalPages  int
+	CurrentPage int
+	NextPage    int
+	LastPage    int
+	PerPage     int
+}
+
+// results mapping
+type MongoResults struct {
+	Status     bool
+	Code       int
+	Database   string
+	Table      string
+	Count      int64
+	Results    []map[string]interface{}
+	Pagination MongoResultPagination
+}
+
+// results error
+type MongoResultsError struct {
+	Status   bool
+	Code     int
+	Database string
+	Table    string
+	Error    string
+}
+
+// initializes the mongo handler driver
+// similar to how constructors work
 func NewMongoHandler() *MongoHandler {
 	host := helpers.GetEnv("MONGO_DB_HOST", "localhost")
 	port := helpers.GetEnv("MONGO_DB_PORT", "27017")
@@ -38,13 +72,14 @@ func NewMongoHandler() *MongoHandler {
 	username := helpers.GetEnv("MONGO_DB_USERNAME", "admin")
 	password := helpers.GetEnv("MONGO_DB_PASSWORD", "admin")
 	useTimestamps, _ := strconv.ParseBool(helpers.GetEnv("HANDLER_USE_TIMESTAMPS", "true"))
+	debug, _ := strconv.ParseBool(helpers.GetEnv("HANDLER_DEBUG", "false"))
 
 	return &MongoHandler{
-		debug:         false,
+		debug:         debug,
 		limit:         0,
 		perPage:       10,
 		page:          1,
-		sort:          []string{},
+		sort:          []primitive.E{},
 		query:         make(map[string]interface{}),
 		host:          host,
 		port:          port,
@@ -60,6 +95,7 @@ func NewMongoHandler() *MongoHandler {
 	}
 }
 
+// handles connectivity
 func (mh *MongoHandler) getConnection() error {
 	if mh.client != nil {
 		return nil
@@ -89,61 +125,81 @@ func (mh *MongoHandler) getConnection() error {
 	return nil
 }
 
+// set the table / collection name
 func (mh *MongoHandler) FromTable(tableName string) *MongoHandler {
 	mh.tableName = tableName
 	return mh
 }
 
+// set table / collection name
 func (mh *MongoHandler) IntoTable(tableName string) *MongoHandler {
 	mh.tableName = tableName
 	return mh
 }
 
+// set db name
 func (mh *MongoHandler) FromDB(dbName string) *MongoHandler {
 	mh.dbName = dbName
 	return mh
 }
 
+// set  db name
 func (mh *MongoHandler) IntoDB(dbName string) *MongoHandler {
 	mh.dbName = dbName
 	return mh
 }
 
-// where contraints mapped
+func (mh *MongoHandler) PerPage(perPage int) *MongoHandler {
+	mh.perPage = perPage
+	return mh
+}
+
+func (mh *MongoHandler) Page(page int) *MongoHandler {
+	mh.page = page
+	return mh
+}
+
+// Where contraint
 func (mh *MongoHandler) Where(field string, operator string, value interface{}) *MongoHandler {
-	operatorMap := map[string]interface{}{
-		"=":     value,
-		"!=":    map[string]interface{}{"$ne": value},
-		"<":     map[string]interface{}{"$lt": value},
-		"<=":    map[string]interface{}{"$lte": value},
-		">":     map[string]interface{}{"$gt": value},
-		">=":    map[string]interface{}{"$gte": value},
-		"_like": map[string]interface{}{"$regex": value, "options": "i"},
+	if field == "_id" {
+		objectID, err := primitive.ObjectIDFromHex(value.(string))
+		if err != nil {
+			return mh
+		}
+		value = objectID
 	}
 
-	if _, ok := operatorMap[operator]; ok {
-		mh.query[field] = operatorMap[operator]
+	mappedValue, err := mapOperators(operator, value)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return mh
 	}
+
+	mh.query[field] = mappedValue
 
 	return mh
 }
 
+// and where constraint
 func (mh *MongoHandler) AndWhere(field, operator string, value interface{}) *MongoHandler {
-	operatorMap := map[string]interface{}{
-		"=":     value,
-		"!=":    map[string]interface{}{"$ne": value},
-		"<":     map[string]interface{}{"$lt": value},
-		"<=":    map[string]interface{}{"$lte": value},
-		">":     map[string]interface{}{"$gt": value},
-		">=":    map[string]interface{}{"$gte": value},
-		"_like": map[string]interface{}{"$regex": value, "options": "i"},
+	if field == "_id" {
+		objectID, err := primitive.ObjectIDFromHex(value.(string))
+		if err != nil {
+
+			return mh
+		}
+		value = objectID
 	}
 
 	and_condition := make(map[string]interface{})
 
-	if _, ok := operatorMap[operator]; ok {
-		and_condition[field] = operatorMap[operator]
+	mappedValue, err := mapOperators(operator, value)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return mh
 	}
+
+	and_condition[field] = mappedValue
 
 	// check for $and contraints in the query
 	if _, ok := mh.query["$and"]; !ok {
@@ -155,6 +211,59 @@ func (mh *MongoHandler) AndWhere(field, operator string, value interface{}) *Mon
 	return mh
 }
 
+// or where
+func (mh *MongoHandler) OrWhere(field, operator string, value interface{}) *MongoHandler {
+	if field == "_id" {
+		objectID, err := primitive.ObjectIDFromHex(value.(string))
+		if err != nil {
+			return mh
+		}
+		value = objectID
+	}
+	or_condition := make(map[string]interface{})
+
+	mappedValue, err := mapOperators(operator, value)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return mh
+	}
+	or_condition[field] = mappedValue
+
+	// check for $or contraints in the query
+	if _, ok := mh.query["$or"]; !ok {
+		mh.query["$or"] = []interface{}{}
+	}
+
+	mh.query["$or"] = append(mh.query["$or"].([]interface{}), or_condition)
+
+	return mh
+}
+
+// ascending / descending sorting -> 1, -1
+func (mh *MongoHandler) Sort(field, order string) *MongoHandler {
+	if mh.sort == nil {
+		mh.sort = []primitive.E{}
+	}
+
+	var sortOrder int32
+	if order == "ASC" {
+		sortOrder = 1
+	} else {
+		sortOrder = -1
+	}
+
+	mh.sort = append(mh.sort, primitive.E{Key: field, Value: sortOrder})
+	return mh
+}
+
+// setting up results limit
+func (mh *MongoHandler) Limit(limit int) *MongoHandler {
+	mh.limit = limit
+	return mh
+}
+
+// just adds created_at and updated_at timestamps
+// upon insertion and or update
 func (mh *MongoHandler) appendTimestamps(data interface{}) interface{} {
 	if !mh.useTimestamps {
 		return data
@@ -194,30 +303,42 @@ func (mh *MongoHandler) Insert(data interface{}) error {
 	}
 
 	if mh.useTimestamps {
-		data = mh.appendTimestamps(data)
+		switch d := data.(type) {
+		case []map[string]interface{}:
+			for i := range d {
+				d[i] = mh.appendTimestamps(d[i]).(map[string]interface{})
+			}
+		case map[string]interface{}:
+			data = mh.appendTimestamps(d).(map[string]interface{})
+		}
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// check if it is slice
-	// of maps
-	if _, ok := data.([]interface{}); ok {
-		_, err := mh.collection.InsertMany(ctx, data.([]interface{}))
+	switch d := data.(type) {
+	case []map[string]interface{}:
+		var interfaceSlice []interface{}
+		for _, item := range d {
+			interfaceSlice = append(interfaceSlice, item)
+		}
+		_, err := mh.collection.InsertMany(ctx, interfaceSlice)
 		if err != nil {
 			return err
 		}
-	} else {
-		// just insert one
-		_, err := mh.collection.InsertOne(ctx, data)
+	case map[string]interface{}:
+		_, err := mh.collection.InsertOne(ctx, d)
 		if err != nil {
 			return err
 		}
+	default:
+		return errors.New("unsupported data type")
 	}
 
 	return nil
 }
 
+// deletes a mongo db
 func (mh *MongoHandler) DropDatabase(dbName string) error {
 	if err := mh.getConnection(); err != nil {
 		return err
@@ -234,6 +355,7 @@ func (mh *MongoHandler) DropDatabase(dbName string) error {
 	return nil
 }
 
+// deletes a mongo collection / table
 func (mh *MongoHandler) DropTable(tableName string) error {
 	if err := mh.getConnection(); err != nil {
 		return err
@@ -250,34 +372,111 @@ func (mh *MongoHandler) DropTable(tableName string) error {
 	return nil
 }
 
-// some type of query builder
-func (mh *MongoHandler) Find(query interface{}) ([]map[string]interface{}, error) {
-	if err := mh.getConnection(); err != nil {
-		return nil, err
+func (mh *MongoHandler) TotalCount() (int64, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	totalCount, err := mh.collection.CountDocuments(ctx, mh.query)
+	if err != nil {
+		// Handle error
+		return 0, err
 	}
 
+	return totalCount, nil
+}
+
+// a query must be provided beforehand
+func (mh *MongoHandler) Find() (MongoResults, MongoResultsError) {
+	// connection to the mongodb server
+	if err := mh.getConnection(); err != nil {
+		return MongoResults{}, mh.newMongoResultsError(500, err.Error())
+	}
+
+	// 5 sec timeout for the context
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cur, err := mh.collection.Find(ctx, query)
+	opts := options.Find()
+	if len(mh.sort) > 0 {
+		opts.SetSort(mh.sort)
+	}
+
+	fmt.Println(mh.query)
+
+	// uses the mh.query chained to the handler
+	cur, err := mh.collection.Find(ctx, mh.query, opts)
 	if err != nil {
-		return nil, err
+		if err == mongo.ErrNoDocuments {
+			return MongoResults{}, mh.newMongoResultsError(404, "No results found")
+		}
+		return MongoResults{}, mh.newMongoResultsError(500, err.Error())
 	}
 	defer cur.Close(ctx)
 
+	// results slice
 	var results []map[string]interface{}
+
+	// decode documents
 	for cur.Next(ctx) {
 		var result map[string]interface{}
 		if err := cur.Decode(&result); err != nil {
-			return nil, err
+			return MongoResults{}, mh.newMongoResultsError(500, err.Error())
 		}
 		results = append(results, result)
 	}
+
+	// iteration errors
 	if err := cur.Err(); err != nil {
-		return nil, err
+		return MongoResults{}, mh.newMongoResultsError(500, err.Error())
 	}
 
-	return results, nil
+	// Get the total count of documents
+	totalCount, err := mh.TotalCount()
+	if err != nil {
+		return MongoResults{}, mh.newMongoResultsError(500, err.Error())
+	}
+
+	// should check if there's anything to be returned
+	if len(results) == 0 {
+		return MongoResults{}, mh.newMongoResultsError(404, "No results found.")
+	}
+
+	// pagination handling here
+	totalPages := (int(totalCount) + mh.perPage - 1) / mh.perPage
+	currentPage := mh.page
+	nextPage := currentPage + 1
+	if nextPage > totalPages {
+		nextPage = 0
+	}
+
+	// the actual results xoxoxo
+	return MongoResults{
+		Status:   true,
+		Code:     200,
+		Database: mh.dbName,
+		Table:    mh.tableName,
+		Count:    totalCount,
+		Results:  results,
+		Pagination: MongoResultPagination{
+			TotalPages:  totalPages,
+			CurrentPage: currentPage,
+			NextPage:    nextPage,
+			LastPage:    totalPages,
+			PerPage:     mh.perPage,
+		},
+	}, MongoResultsError{}
+}
+
+// map the erros and the code
+// to avoid repetitions and code
+// cluttering
+func (mh *MongoHandler) newMongoResultsError(code int, err string) MongoResultsError {
+	return MongoResultsError{
+		Status:   false,
+		Code:     code,
+		Database: mh.dbName,
+		Table:    mh.tableName,
+		Error:    err,
+	}
 }
 
 // takes a criteria
@@ -314,4 +513,24 @@ func (mh *MongoHandler) Delete(filter interface{}) error {
 	}
 
 	return nil
+}
+
+// map the operator with the value
+// to reduce code cluttering
+func mapOperators(operator string, value interface{}) (interface{}, error) {
+	operatorMap := map[string]interface{}{
+		"=":     value,
+		"!=":    map[string]interface{}{"$ne": value},
+		"<":     map[string]interface{}{"$lt": value},
+		"<=":    map[string]interface{}{"$lte": value},
+		">":     map[string]interface{}{"$gt": value},
+		">=":    map[string]interface{}{"$gte": value},
+		"_like": map[string]interface{}{"$regex": value, "options": "i"},
+	}
+
+	if _, ok := operatorMap[operator]; ok {
+		return operatorMap[operator], nil
+	}
+
+	return nil, errors.New("unrecognized operator")
 }
--- main.go
+++ main.go
@@ -28,19 +28,66 @@ func init() {
 func main() {
 	fmt.Println(apiPort)
 
-	// jsonData := map[string]interface{}{
+	// record := map[string]interface{}{
 	// 	"firstName": "Matthew",
 	// 	"lastName":  "Something",
 	// 	"age":       21,
 	// }
 
-	err := driver.
+	data, err := driver.
 		NewMongoHandler().
 		FromDB("alexanderdth").
 		FromTable("my-table").
-		AndWhere("alex", "!=", "someone").
-		AndWhere("age", "!=", "someone")
+		Where("firstName", "!=", "HfEOTJ").
+		PerPage(10).
+		Page(50).
+		Find()
 
-	fmt.Println(err)
+	if err.Error != "" {
+		// Handle the error
+		fmt.Println("Error:", err)
+	} else {
+		// Print the data
+		fmt.Println("Status:", data.Status)
+		fmt.Println("Code:", data.Code)
+		fmt.Println("Database:", data.Database)
+		fmt.Println("Table:", data.Table)
+		fmt.Println("Count:", data.Count)
+		fmt.Println("Pagination:")
+		fmt.Println("\tTotalPages:", data.Pagination.TotalPages)
+		fmt.Println("\tCurrentPage:", data.Pagination.CurrentPage)
+		fmt.Println("\tNextPage:", data.Pagination.NextPage)
+		fmt.Println("\tLastPage:", data.Pagination.LastPage)
+		fmt.Println("\tPerPage:", data.Pagination.PerPage)
 
+	}
+
+	// rand.Seed(time.Now().UnixNano())
+
+	// // Create a slice to store the records
+	// var records []map[string]interface{}
+
+	// // Generate 40 records with random data
+	// for i := 0; i < 500; i++ {
+	// 	record := make(map[string]interface{})
+	// 	record["firstName"] = randomString(6)
+	// 	record["lastName"] = randomString(8)
+	// 	record["age"] = rand.Intn(50) + 18 // Random age between 18 and 67
+	// 	records = append(records, record)
+	// }
+
+	// err := driver.NewMongoHandler().IntoDB("alexanderdth").IntoTable("my-table").Insert(record)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 }
+
+// func randomString(length int) string {
+// 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// 	b := make([]byte, length)
+// 	for i := range b {
+// 		b[i] = charset[rand.Intn(len(charset))]
+// 	}
+// 	return string(b)
+// }
