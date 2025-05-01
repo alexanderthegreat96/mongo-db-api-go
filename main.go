108c0f14c38549e72bbf042f38328fb8e126c61d
alexanderthegreat96 <alexandrulupaescu@gmail.com>
2025-05-02 00:35:57 +0300

    applied some improvements

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func init() {
 	logger = log.New(os.Stdout, "[MONGO-API]: ", log.Ldate|log.Ltime)
 	env := envparser.NewEnvParser(envparser.WithFilename(".env"), envparser.WithRootPath(true))
 
-	versionNumber := "v1.6.0"
+	versionNumber := "v1.0.6"
 	mongoApiBanner := figure.NewColorFigure(fmt.Sprintf("MongoAPI %s", versionNumber), "", "blue", false)
 	mongoApiBanner.Print()
 	fmt.Println()
